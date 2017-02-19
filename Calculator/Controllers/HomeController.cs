7ed78add621f9354a4ce6dafad41b55efc170663
swati <swati@LAPTOP-R02QAOND>
2017-02-19 01:53:05 +0000

    Add project files.

---
+++ Calculator/Controllers/HomeController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Calculator.Models;
+
+namespace Calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public ActionResult Index()
+        {
+
+            return View();
+         
+        }
+
+        [HttpPost]
+        public ActionResult Result(QuoteRequest quoteRequest)
+
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                quoteRequest.principal = quoteRequest.borrow - quoteRequest.initialDeposit;
+
+                quoteRequest.years = quoteRequest.durationInMonths / 12;
+
+               
+                
+                //Property Value
+
+                quoteRequest.propertyValue = quoteRequest.initialDeposit + quoteRequest.borrow;
+
+
+                //r=rate/month
+                double r = quoteRequest.apr / 100 / 12;
+
+                double d = Math.Pow((1 + r), quoteRequest.durationInMonths) - 1;
+
+                quoteRequest.monthlyPayment = (r + (r / d)) * quoteRequest.principal;
+
+
+
+
+
+                //Calculate  monthly payment for interest only mortgage
+
+                double i = quoteRequest.principal * quoteRequest.apr;
+                double f = i / 12;
+                quoteRequest.monthIOPayment = f / 100;
+
+
+
+
+                //Calculate total Interest for IO mortgage
+
+                quoteRequest.totalinterestIO = quoteRequest.monthIOPayment * quoteRequest.durationInMonths;
+
+
+                //Calculate total Loan Repayment
+                quoteRequest.totalPayment = quoteRequest.monthlyPayment * quoteRequest.durationInMonths;
+
+
+
+                //Calculate TotalInterest for Repayment
+
+                quoteRequest.totalInterest = quoteRequest.totalPayment - quoteRequest.principal;
+
+
+
+
+                //Compare  interest only vs repayment
+
+                quoteRequest.Compare = quoteRequest.totalPayment - quoteRequest.totalinterestIO;
+
+
+                return View("Result", quoteRequest);
+            }
+            else
+            {
+                return View("Index");
+            }
+
+
+        }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Calculator/Models/QuoteRequest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Calculator.Models
+{
+    public class QuoteRequest
+
+    {
+        [Required (ErrorMessage ="Please enter the mortgage amount")]
+        [Display(Name ="Mortgage Amount") ]
+        public double borrow { get; set; }
+
+        [Required (ErrorMessage ="Please enter an initial Deposit")]
+        [Display(Name ="Initial Deposit")]
+        public double initialDeposit { get; set; }
+
+        [Required (ErrorMessage ="Please enter the annual rate of interest")]
+        [Display(Name = "Annual Rate of Interest")]
+        public double apr { get; set; }
+
+        [Required (ErrorMessage ="Please enter the mortgage term(months)")]
+        [Display(Name = "Mortgage Term (months)")]
+        public double durationInMonths { get; set; }
+
+        
+        public double principal { get; set; }
+        public double years { get; set; }
+        public double propertyValue { get; set; }
+        public double monthlyPayment { get; set; }
+        public double monthIOPayment { get; set; }
+        public double totalinterestIO { get; set; }
+        public double totalInterest { get; set; }
+        public double totalPayment { get; set; }
+        public double Compare { get; set; }
+    }
+}
